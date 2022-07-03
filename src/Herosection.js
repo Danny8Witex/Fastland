c20b7ec4b069abdb2105906fe63a035b496571a7
Daniel <adeleyed53@gmail.com>
2022-07-02 18:28:49 -0700

    responsive update & nav bar alignment

--- src/Herosection.js
+++ src/Herosection.js
@@ -3,14 +3,16 @@ function Herosection(){
 return(
     <div className="background">
     <div className="background2">
+    <nav className='nav'>
+    <h1 className='logo'>FASTLAND</h1>
     <ul>
-    <li className='logo'>FASTLAND</li>
     <li>Home</li>
     <li>About Us</li>
     <li>Service</li>
     <li><button className='signup'>Sign Up</button></li>
     </ul>
-    <h1>We are a goal oriented<br></br>Project Management Agency</h1>
+    </nav>
+    <h1 classNaame='headrText'>We are a goal oriented<br></br>Project Management Agency</h1>
     <div className='Herotext'>
     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
     <button className='contactus'>Contact Us</button>
